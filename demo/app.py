3394f7991e0544fd8cd38e02662d3bf8bc65dfe4
WuShaobei <1393335492@qq.com>
2022-10-05 09:57:38 +0800

    local

--- demo/app.py
+++ demo/app.py
@@ -227,6 +227,6 @@ def reviewFun():
 
 
 if __name__ == '__main__':
-    app.run(debug=True,host='localhost',port=1432)
-    # app.run(debug=True,host='127.0.0.1',port=1423)
+    # app.run(debug=True,host='localhost',port=1432)
+    app.run(debug=True,host='127.0.0.1',port=1423)
     # app.run(debug=True,host='0.0.0.0',port=1423)
